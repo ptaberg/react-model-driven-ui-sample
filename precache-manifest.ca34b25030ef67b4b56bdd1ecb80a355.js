630887c1a1e1a924d93653e46464a75c58f45c8a
Kolja Ptaberg <ptashym@gmail.com>
2020-05-19 13:35:21 +0300

    Updates

---
+++ precache-manifest.ca34b25030ef67b4b56bdd1ecb80a355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d048895b11265db9f51b12d884b26ebe",
+    "url": "/react-model-driven-ui-sample/index.html"
+  },
+  {
+    "revision": "90672fd7b8b22a4e95f1",
+    "url": "/react-model-driven-ui-sample/static/css/main.7ec1b171.chunk.css"
+  },
+  {
+    "revision": "bbb2635f3ee5f6c35202",
+    "url": "/react-model-driven-ui-sample/static/js/2.edb63383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-model-driven-ui-sample/static/js/2.edb63383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90672fd7b8b22a4e95f1",
+    "url": "/react-model-driven-ui-sample/static/js/main.32fb3987.chunk.js"
+  },
+  {
+    "revision": "1ef2bdc3bb7e3755d3f8",
+    "url": "/react-model-driven-ui-sample/static/js/runtime-main.e42db673.js"
+  }
+]);
